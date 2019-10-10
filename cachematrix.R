eda13c98e969971a5046cc999c7ba93f40ec9c8a
Kwabena-dev <56121659+Kwabena-dev@users.noreply.github.com>
2019-10-10 19:41:38 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,55 +1,45 @@
 ## setwd("~/RStudio samples/coursera course 2")
 
 Firstly I input x as a matrix
-My solved value "s" as a null
+
+My solved value "k" as a null
+
 Every reference was set to "mean" to "solve"
+
 makeCacheMatrix <- function(x = matrix()) {
-
-  s <- NULL
-
-  set <- function(y) {
-
-    x <<- y
-
-    s <<- NULL
-
-  }
-
-  get <- function() x
-
-  setsolve <- function(solve) s <<- solve
-
-  getsolve <- function() s
-
-  list(set = set, get = get,
-
-       setsolve = setsolve,
-
-       getsolve = getsolve)
-
+    
+    k <- NULL
+    set <- function(y) {
+        x <<- y
+        k <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) k <<- solve
+    getsolve <- function() k
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-Likewise I set every reference "mean" to "solve" and "m" to "s"
+
+
+
+Likewise I set every reference "mean" to "solve" and "m" to "k"
+
 ## This was done to return a matrix that is the inverse of 'x'
 
+
+
 cacheSolve <- function(x, ...) {
-        
-        s <- x$getsolve()
-
-  if(!is.null(s)) {
-
-    message("getting inversed matrix")
-
-    return(s)
-
-  }
-
-  data <- x$get()
-
-  s <- solve(data, ...)
-
-  x$setsolve(s)
-
-  s
+    
+    k <- x$getsolve()
+    if(!is.null(k)) {
+        message("getting inversed matrix")
+        return(k)
+    }
+    data <- x$get()
+    k <- solve(data, ...)
+    x$setsolve(k)
+    k    
 }
